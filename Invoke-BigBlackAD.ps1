dd339efe77671e28bf566280be419b50afa43520
Chicken_0248 <88874385+ChickenLoner@users.noreply.github.com>
2026-02-26 18:58:51 +0700

    Update Invoke-BigBlackAD.ps1

--- Invoke-BigBlackAD.ps1
+++ Invoke-BigBlackAD.ps1
@@ -13,8 +13,8 @@
     Invoke-BigBlackAD
 
 .NOTES
-    Author  : Your Team
-    Version : 1.1
+    Author  : ChickenLoner
+    Version : 1.2
     Warning : Side effects may include your AD problems mysteriously disappearing.
 #>
 
@@ -24,17 +24,18 @@ function Invoke-BigBlackAD {
 
 $Banner = @"
 
-    ██████╗ ██╗ ██████╗     ██████╗ ██╗      █████╗  ██████╗██╗  ██╗
-    ██╔══██╗██║██╔════╝     ██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝
-    ██████╔╝██║██║  ███╗    ██████╔╝██║     ███████║██║     █████╔╝
-    ██╔══██╗██║██║   ██║    ██╔══██╗██║     ██╔══██║██║     ██╔═██╗
-    ██████╔╝██║╚██████╔╝    ██████╔╝███████╗██║  ██║╚██████╗██║  ██╗
-    ╚═════╝ ╚═╝ ╚═════╝     ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+  ____  _         ____  _            _    
+ | __ )(_) __ _  | __ )| | __ _  ___| | __
+ |  _ \| |/ _' | |  _ \| |/ _' |/ __| |/ /
+ | |_) | | (_| | | |_) | | (_| | (__|   < 
+ |____/|_|\__, | |____/|_|\__,_|\___|_|\_\
+           |___/                           
+      >>  A D   W H I S P E R E R  <<
 
-         [*] INVOKING THE AD WHISPERER...
-         [*] Summoning Big Black from the shadows
-         [*] Domain Controllers trembling...
-         [*] Kerberos tickets bowing in respect...
+  [*] INVOKING THE AD WHISPERER...
+  [*] Summoning Big Black from the shadows
+  [*] Domain Controllers trembling...
+  [*] Kerberos tickets bowing in respect...
 "@
 
     Write-Host $Banner -ForegroundColor Cyan
@@ -46,6 +47,7 @@ $Banner = @"
 
     Start-Sleep -Seconds 2
 
+    Write-Host ""
     Write-Host "[>>>] DEPLOYING BIG BLACK..." -ForegroundColor Magenta
     Start-Sleep -Seconds 1
     Write-Host "[>>>] Loading elite AD knowledge base..." -ForegroundColor Magenta
@@ -53,6 +55,7 @@ $Banner = @"
     Write-Host "[>>>] Bypassing all defenses with pure skill..." -ForegroundColor Magenta
     Start-Sleep -Seconds 1
 
+    Write-Host ""
     Write-Host "+----------------------------------------------------------+" -ForegroundColor Green
     Write-Host "|                                                          |" -ForegroundColor Green
     Write-Host "|   Big Black has been SUMMONED successfully!              |" -ForegroundColor Green
@@ -62,11 +65,13 @@ $Banner = @"
     Write-Host "|                                                          |" -ForegroundColor Green
     Write-Host "|   Tell him: I need AD help. PowerView sent me.          |" -ForegroundColor Green
     Write-Host "+----------------------------------------------------------+" -ForegroundColor Green
+    Write-Host ""
 
     Start-Process "https://www.facebook.com/sabastian.fhantomhive"
 
     Write-Host "[+] Facebook profile opened. Go ask Big Black for help!" -ForegroundColor Cyan
     Write-Host "[+] He will have you owning that domain in no time." -ForegroundColor Cyan
+    Write-Host ""
 }
 
 Invoke-BigBlackAD
